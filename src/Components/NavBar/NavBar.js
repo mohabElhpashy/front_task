f52c49317dd7e93d1e11547cbb67f4f13208507f
mohabelhbashy <mohab.hafez332@gmail.com>
2021-05-04 20:35:07 +0200

    LandScape

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,118 +1,137 @@
-import React, { useState, useEffect } from "react";
-import "./NavBar.css";
-import {
-  BsFillBellFill,
-  BsFillPersonFill,
-  BsFillChatDotsFill,
-} from "react-icons/bs";
-import { MdDashboard } from "react-icons/md";
-import Notfi from "../Notification/Notification";
-import Message from "../Messages/Messages";
-import PRofile from "../Profile/Profile";
-import DaShBoard from "../DashBoard/Dashboard";
-import Weeather from "../Weather/Weather";
+    import React, { useState, useEffect } from "react";
+    import "./NavBar.css";
+    import {
+      BsFillBellFill,
+      BsFillPersonFill,
+      BsFillChatDotsFill,
+    } from "react-icons/bs";
+    import { MdDashboard } from "react-icons/md";
+    import Notfi from "../Notification/Notification";
+    import Message from "../Messages/Messages";
+    import PRofile from "../Profile/Profile";
+    import DaShBoard from "../DashBoard/Dashboard";
+    // import Weeather from "../Weather/Weather";
 
-import Aos from "aos";
-import "aos/dist/aos.css";
-import { Provider } from "../../hooks/Provider";
+    import Aos from "aos";
+    import "aos/dist/aos.css";
+    import { Provider } from "../../hooks/Provider";
 
-function NavBar({ checkMainState }) {
-  const [notification, setnotification] = useState(false);
-  const [Messages, setMessages] = useState(false);
-  const [Profile, setProfile] = useState(false);
-  const [DashBoard, setDashBoard] = useState(false);
-  const [Weather, setWeather] = useState(false);
+    function NavBar({ checkMainState }) {
+      const [notification, setnotification] = useState(false);
+      const [Messages, setMessages] = useState(false);
+      const [Profile, setProfile] = useState(false);
+      const [DashBoard, setDashBoard] = useState(false);
+      const [Weather, setWeather] = useState(false);
 
-  const CloseProvider = () => {
-    setDashBoard(false);
-    setWeather(true);
-  };
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  }, []);
+      const CloseProvider = () => {
+        setDashBoard(false);
+        setWeather(true);
+      };
+      useEffect(() => {
+        Aos.init({ duration: 2000 });
+      }, []);
 
-  return (
-    <div>
-      <nav data-aos="fade-bottom" className="NavBar">
-        <ul>
-          <li
-            onClick={() => {
-              setDashBoard(!DashBoard);
-              setProfile(false);
-              setMessages(false);
-              setnotification(false);
-            }}
-          >
-            {DashBoard ? (
-              <MdDashboard color="blue" size={35} />
-            ) : (
-              <MdDashboard color="white" size={35} />
-            )}
-          </li>
-          <li
-            onClick={() => {
-              setnotification(!notification);
-              setMessages(false);
-              setProfile(false);
-              setDashBoard(false);
-              //   document.getElementById("two").style.color = "blue";
-            }}
-          >
-            {notification ? (
-              <BsFillBellFill color="blue" size={35} />
-            ) : (
-              <BsFillBellFill color="white" size={35} />
-            )}
-          </li>
-          <li
-            onClick={() => {
-              setMessages(!Messages);
-              setnotification(false);
-              setProfile(false);
-              setDashBoard(false);
-            }}
-          >
-            {Messages ? (
-              <BsFillChatDotsFill color="blue" size={35} />
-            ) : (
-              <BsFillChatDotsFill color="white" size={35} />
-            )}
-          </li>
-          <li>
-            {Profile ? (
-              <BsFillPersonFill
+      return (
+        <div>
+          <nav data-aos="fade-bottom" className="NavBar">
+            <ul>
+              <li
                 onClick={() => {
-                  setProfile(!Profile);
+                  setDashBoard(!DashBoard);
+                  setProfile(false);
                   setMessages(false);
+                  setnotification(false);
+                  setWeather(false);
+                }}
+              >
+                {DashBoard ? (
+                  <MdDashboard color="blue" size={35} />
+                ) : (
+                  <MdDashboard color="white" size={35} />
+                )}
+                <span className='spaaan'>Dashboard</span>
+              </li>
+              <li
+                onClick={() => {
+                  setnotification(!notification);
+                  setMessages(false);
+                  setProfile(false);
                   setDashBoard(false);
+                  setWeather(false);
 
-                  setnotification(false);
+                  //   document.getElementById("two").style.color = "blue";
                 }}
-                color="blue"
-                size={35}
-              />
-            ) : (
-              <BsFillPersonFill
+              >
+                {notification ? (
+                  <BsFillBellFill color="blue" size={35} />
+                ) : (
+                  <BsFillBellFill color="white" size={35} />
+                )}
+                            <span className='spaaan'>notification</span>
+
+              </li>
+              <li
                 onClick={() => {
-                  setProfile(!Profile);
-                  setMessages(false);
+                  setMessages(!Messages);
                   setnotification(false);
+                  setProfile(false);
+                  setDashBoard(false);
+                  setWeather(false);
+
                 }}
-                color="white"
-                size={35}
-              />
-            )}
-          </li>
-        </ul>
-      </nav>
-      <Notfi checknoti={notification} />
-      <Message CheckMessage={Messages} />
-      <PRofile CheckProfile={Profile} />
-      <Provider value={{ DAshState: "DashBoard", myfun: CloseProvider }}>
-        <DaShBoard CheckDash={DashBoard} />
-      </Provider>
-      <Weeather CkeckWeather={Weather} />
-    </div>
-  );
-}
-export default NavBar;
+              >
+                {Messages ? (
+                  <><BsFillChatDotsFill color="blue" size={35}  />
+                  </>
+                ) : (
+                  <BsFillChatDotsFill color="white" size={35} />
+                )}
+              <span className='spaaan'>Messages</span>
+              <span style={{position:'absolute',textAlign:'center',width:'25px',borderRadius:'100%',
+            backgroundColor:'red',color:'white'}}>3</span>
+
+
+              </li>
+              <li>
+                {Profile ? (
+                  <BsFillPersonFill
+                    onClick={() => {
+                      setProfile(!Profile);
+                      setMessages(false);
+                      setDashBoard(false);
+                      setWeather(false);
+
+                      setnotification(false);
+                    }}
+                    color="blue"
+                    size={35}
+                  />
+                ) : (
+                  <BsFillPersonFill
+                    onClick={() => {
+                      setProfile(!Profile);
+                      setMessages(false);
+                      setnotification(false);
+                      setWeather(false);
+
+                    }}
+                    color="white"
+                    size={35}
+                  />
+                )}
+          <span className='spaaan'>Profile</span>
+
+              </li>
+            </ul>
+          </nav>
+          <Notfi checknoti={notification} />
+          <Message CheckMessage={Messages} />
+          <PRofile CheckProfile={Profile} />
+          <Provider value={{ DAshState: "DashBoard", myfun: CloseProvider }}>
+            <DaShBoard CheckDash={DashBoard} />
+          </Provider>
+          {/* <Weeather CkeckWeather={Weather} /> */}
+        </div>
+      );
+    }
+    export default NavBar;
--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,50 +1,73 @@
-  import React, { useEffect, useState } from "react";
-  import "./Weather.css";
-  import Axios from "axios";
-  import { TiWeatherPartlySunny } from "react-icons/ti";
-  function Weather({ CkeckWeather }) {
-    const [MaxTem, SetMaxTemp] = useState();
-    const [LowTemp, SetLowTemp] = useState();
-    const [City, SetCity] = useState('giza');
-    const [Country, SetCountry] = useState('eg');
-    const [humidity, sethumidity] = useState();
-    const [loading, setLoading] = useState(false);
-    const [Icon, setIcon] = useState("");
-    const [description, setdescription] = useState("");
+  // import React, { useEffect, useState } from "react";
+  // import "./Weather.css";
+  // import Axios from "axios";
+  // import { TiWeatherPartlySunny } from "react-icons/ti";
+  // function Weather({ CkeckWeather }) {
+  //   const [MaxTem, SetMaxTemp] = useState();
+  //   const [LowTemp, SetLowTemp] = useState();
+  //   const [Country, SetCountry] = useState({City:'Giza',Coun:'eg'});
+  //   const [humidity, sethumidity] = useState();
+  //   const [loading, setLoading] = useState(false);
+  //   const [description, setdescription] = useState("");
+  //   const [changeareabutt,setchangearebutt]=useState({City:'Giza',Coun:'eg'})
 
-    const Api_Key = "5ff77894dfd5d8d1a3a041520439e9bb";
 
-    useEffect(() => {
-      <input onChange={(e) => SetCity(e.target.value)} />;
-      Axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${City},${Country}&appid=${Api_Key}`
-      ).then((res) => {
-        SetMaxTemp(res.data.main.temp_max);
-        SetLowTemp(res.data.main.temp_min);
-        sethumidity(res.data.main.humidity);
-        setdescription(res.data.weather[0].description);
 
-        setLoading(true);
-        console.log(res.data);
-      });
-    }, []);
-    return CkeckWeather && loading === true ? (
-      <div style={{ height: "350px" }} className="Weather_div">
-        <h2>
-          <TiWeatherPartlySunny color="white" size={100} />
-        </h2>
-        <div style={{color:'white',fontSize:'40px'}}>{(humidity *100)/100}&deg;</div>
-        <div className="maina">
-          <div>{Math.floor(MaxTem - 273.15)}&deg;</div>
-          <div>{Math.floor(LowTemp - 273.15)}&deg;</div>
-        </div>
-        <h2 style={{color:'white'}}>{description}</h2>
-      </div>
-    ) : (
-      <div style={{ height: "0" }} className="Weather_div">
-        {" "}
-      </div>
-    );
-  }
-  export default Weather;
-  //   `http://api.weatherstack.com/current?access_key=c50ec072ab788fe0254932d3dd1b9617&query=${30.068737},${31.282093}&forecast_days=1&hourl=1`
+  //   const Api_Key = "5ff77894dfd5d8d1a3a041520439e9bb";
+  //   const ChangeArea=()=>{
+  //     setchangearebutt(Country)
+  //   }
+  //   if('serviceWorker' in navigator){
+  //     console.log('it is done')
+  //   }
+  //   else{
+  //     console.log('kskks')
+  //   }
+
+  //   useEffect(() => {
+  //     Axios.get(
+  //       //call open wether api
+  //       `http://api.openweathermap.org/data/2.5/weather?q=${changeareabutt.City},${changeareabutt.Coun}&appid=${Api_Key}`
+  //     ).then((res) => {
+  //       SetMaxTemp(res.data.main.temp_max);
+  //       SetLowTemp(res.data.main.temp_min);
+  //       sethumidity(res.data.main.humidity);
+  //       setdescription(res.data.weather[0].description);
+
+  //       setLoading(true);
+  //       console.log(res.data);
+  //     });
+      
+  //   }, [changeareabutt]);
+  //   // const Change=()=>{
+  //   //   SetCountry(ChangeAr)
+  //   // }
+  //   return CkeckWeather && loading === true ? (
+  //     <div style={{ height: "400px" }} className="Weather_div">
+      
+  //        <div className="div_hava_input">
+  //          <input type='text' value={Country.City}  onChange={e=>SetCountry({City:e.target.value})} 
+  //           style={{backgroundColor:'transparent'}} placeholder='City'/>,,
+  //         <input  type='text' value={Country.Coun} onChange={e=>SetCountry({Coun:e.target.value})} 
+  //          style={{backgroundColor:'transparent'}} placeholder='Country'/>
+  //         <button onClick={ChangeArea}>Get Weather</button>
+  //         </div>
+
+  //       {/* <hr/> */}
+  //       <h2>
+  //         <TiWeatherPartlySunny color="white" size={100} />
+  //       </h2>
+  //       <div style={{color:'white',fontSize:'40px',fontStyle:'italic',fontWeight:'bold'}}>{humidity }%</div>
+  //       <div className="maina">
+  //         <div>{Math.floor(MaxTem - 273.15)}&deg;<span >max</span></div>
+  //         <div>{Math.floor(LowTemp - 273.15)}&deg; <span >min</span></div>
+  //       </div>
+  //       <h2 style={{color:'white',fontSize:'30px',fontStyle:'italic',fontWeight:'bold'}}>{description}</h2>
+  //     </div>
+  //   ) : (
+  //     <div style={{ height: "0" }} >
+  //       {" "}
+  //     </div>
+  //   );
+  // }
+  // export default Weather;
